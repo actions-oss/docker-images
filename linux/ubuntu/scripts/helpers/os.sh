675fe7d35e44a1f74139f46a7fc3bc7c32528bfb
Jakub Panek <me@panekj.dev>
2025-01-27 12:15:47 +0000

    migrate docker-images

---
+++ linux/ubuntu/scripts/helpers/os.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+# SPDX-License-Identifier: MIT
+################################################################################
+##  File:  install-helpers.sh
+##  Desc:  Helper functions for installing tools
+################################################################################
+
+function isUbuntu18() {
+  . /etc/os-release
+  [[ "${VERSION_ID}" =~ ^18\.(.*)$ ]]
+}
+
+function isUbuntu20() {
+  . /etc/os-release
+  [[ "${VERSION_ID}" =~ ^20\.(.*)$ ]]
+}
+
+function isUbuntu22() {
+  . /etc/os-release
+  [[ "${VERSION_ID}" =~ ^22\.(.*)$ ]]
+}
+
+function isUbuntu24() {
+  . /etc/os-release
+  [[ "${VERSION_ID}" =~ ^22\.(.*)$ ]]
+}
+
+function isUbuntuVer() {
+  local ver=$1
+  . /etc/os-release
+  [[ "${VERSION_ID}" =~ ^$ver\.(.*)$ ]]
+}
+
+function getOSVersionLabel() {
+  . /etc/os-release
+  echo "${VERSION_CODENAME}"
+}
